fbcaf9ebebbcf7627b90b6c2988a5c8c6202cf96
Nikola Miocevic <36111841+Miocevic@users.noreply.github.com>
2020-01-20 04:15:31 +0100

    login screen works now

--- adminPanelLogin.php
+++ adminPanelLogin.php
@@ -1,6 +1,32 @@
 <?php
+    if(isset($_POST['adminLogin']))
+    {
+        $adminUsername=$_POST['ausername'];
+        $adminPassword=$_POST['apassword'];
 
+        if(empty($adminUsername) || empty($adminPassword))
+            echo "<h1>You have to insert both username and password!</h1>";
+           
+        $conn=new mysqli("localhost","root","","companyannualleave");
+        $sql= "SELECT * FROM admins WHERE adminUsername='$adminUsername' AND adminPassword='$adminPassword'";
+        $result= $conn->query($sql);
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
 
+        foreach($rows as $row)
+            echo "Welcome back ".$row['adminName']." ".$row['adminSurname']."!";
+        
+        $conn2=new mysqli("localhost","root","","companyannualleave");
+        $sql2= "SELECT * FROM employees WHERE employeeAnnualRequest = true";
+        $result2= $conn2->query($sql2);
+        $rows2 = $result2->fetch_all(MYSQLI_ASSOC);
+
+        echo "<br>This is a list of employees who sent Annual Request: <br><br>";
+        foreach($rows2 as $row)
+        {
+            echo $row['employeeName']." ".$row['employeeSurname']." [".$row['employeePosition']."]<br>";
+        }
+
+    }
 ?>
 
 <!DOCTYPE html>
